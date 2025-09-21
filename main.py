cb00e02ef732935195b73700a03c396296c8b6d9
stefanosgd <sgdemetriou@gmail.com>
2025-09-21 16:03:56 +0100

    Add player object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -7,11 +8,16 @@ def main():
     fps_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    # Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color=(0,0,0))
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = fps_clock.tick(60) / 1000
 
